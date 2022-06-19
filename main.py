dbe195db3be6925f2b2140584f7535c48b6e4edb
Shawn Azar <cydiasa@gmail.com>
2022-06-19 13:52:27 -0600

    updated

--- main.py
+++ main.py
@@ -1,13 +1,15 @@
 """
 iRacing-Cleanup-Setups
-After installing new vehicle setups in iRacing, I accidentally downloaded other things into my
-vehicles folder. This script goes to the given path and removes anything that is not `-Current-`
-or has an `.sto` extension
+After installing new vehicle setups in iRacing, I accidentally downloaded
+other things into my vehicles folder. This script goes to the given path
+and removes anything that is not `-Current-` or has an `.sto` extension
 """
 
-import os
 import argparse
+import os
+
 from dotenv import load_dotenv
+
 load_dotenv()
 
 parser = argparse.ArgumentParser()
@@ -15,30 +17,31 @@ parser.add_argument(
     '--dryrun',
     default=True,
     action='store_true',
-    help="True by default. Pass in false to delete files"
+    help='True by default. Pass in false to delete files',
 )
-parser.add_argument(
-    '--dry-run',
-    default=True,
-    action=argparse.BooleanOptionalAction
-)
+parser.add_argument('--dry-run', default=True,
+                    action=argparse.BooleanOptionalAction)
 args = parser.parse_args()
-root_path = os.getenv("IRACING_INSTALL_PATH")
 
-print("Script Start")
+root_path = os.getenv(
+    'IRACING_INSTALL_PATH', os.path.expanduser(
+        '~') + '\\Documents\\iRacing\\setups'
+)
+
+print('Script Start')
 if args.dry_run is True:
-    print("Dry run -- Files will not be deleted")
+    print('Dry run -- Files will not be deleted')
 
 for folders in os.listdir(path=root_path):
-    sub_folder_path = root_path + "\\" + folders
+    sub_folder_path = root_path + '\\' + folders
     for files in os.listdir(path=sub_folder_path):
-        file_path = sub_folder_path + "\\" + files
-        if files.lower() != "-current-" and not files.lower().endswith(".sto"):
+        file_path = sub_folder_path + '\\' + files
+        if files.lower() != '-current-' and not files.lower().endswith('.sto'):
             if not os.path.isdir(file_path):
-                print("Found: " + file_path)
+                print('Found: ' + file_path)
                 if not args.dry_run:
                     if os.path.exists(file_path) and os.path.isfile(file_path):
-                        print("Deleting: " + file_path)
+                        print('Deleting: ' + file_path)
                         os.remove(file_path)
 
-print("Script Complete")
+print('Script Complete')
